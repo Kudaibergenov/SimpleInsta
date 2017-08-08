dcdb3247eb5b90fddbe87f40e15d277097924bc9
Zhanuzak Kudaibergenov <kudaibergenov.zhanuzak@gmail.com>
2017-08-09 01:53:03 +0600

    SimpleInsta

---
+++ app/Http/Controllers/InstaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InstaController extends Controller
+{
+    public function index(Request $request){
+    	$items = [];
+
+    	if ($request->has('username')) {
+    		$client = new \GuzzleHttp\Client();
+
+    		$url = sprintf('https://www.instagram.com/%s/media', $request->input('username'));
+    		$response = $client->get($url);
+    		$items = json_decode((string) $response->getBody(), true)['items'];
+    	}
+    	return view ('insta', compact('items'));
+    }
+}
